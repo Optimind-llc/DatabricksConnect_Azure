7db899686b0c9f7fb7548c4775dc60eb60e205f7
GitHub Action <action@github.com>
2021-02-24 07:19:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/laurenfrost/databricksconnectazure/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.laurenfrost.databricksconnectazure.services
+
+import com.github.laurenfrost.databricksconnectazure.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/laurenfrost/databricksconnectazure/services/MyProjectService.kt
+++ src/main/kotlin/com/github/laurenfrost/databricksconnectazure/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.laurenfrost.databricksconnectazure.services
 
+import com.github.laurenfrost.databricksconnectazure.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
